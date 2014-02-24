7efc8e531bffd9d8a605837b94475d3cfff63c65
keco1249 <kcolin1@gmail.com>
2014-02-24 11:27:47 -0700

    case class

--- src/main/scala/com/reactor/dumbledore/notifications/NotificationArgs.scala
+++ src/main/scala/com/reactor/dumbledore/notifications/NotificationArgs.scala
@@ -4,13 +4,11 @@ import com.reactor.patterns.pull.FlowControlArgs
 
 import akka.actor.ActorRef
 
-class NotificationArgs(serviceActor:ActorRef) extends FlowControlArgs{
+case class NotificationArgs(serviceActor:ActorRef) extends FlowControlArgs{
   
   override def workerArgs(): FlowControlArgs ={
     val newArgs = new NotificationArgs(serviceActor)
     newArgs.addMaster(master)
     return newArgs
   }
-  
-  def serviceActor():ActorRef = this.serviceActor
 }
