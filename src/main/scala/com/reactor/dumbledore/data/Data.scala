1e0af495bbb719ba400d4fa260fe30180c8f00ce
keco1249 <kcolin1@gmail.com>
2014-03-06 17:55:59 -0700

    missing files

---
+++ src/main/scala/com/reactor/dumbledore/data/Data.scala
@@ -0,0 +1,37 @@
+package com.reactor.dumbledore.data
+
+import java.util.ArrayList
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
+import com.fasterxml.jackson.databind.JsonNode
+import scala.collection.JavaConversions._
+import com.reactor.prime.user.UserCredentials
+
+/** String mapped to ListBuffer */
+case class ListSet[T](card_id:String, set_data:ListBuffer[T])
+
+
+/** Parameters Map[String,String] */
+case class Parameters(map:Map[String, String]){
+  
+  def this(creds:UserCredentials){
+    this(Map[String,String]())
+    if(creds.udid != null)
+      map.put("udid", creds.udid)
+    if(creds.location != null){
+      map.put("lat", creds.location.lat.toString)
+      map.put("long", creds.location.long.toString)     
+      map.put("loc", creds.location.lat.toString+","+creds.location.long.toString)
+    }
+  }
+}
+
+//implicit def other(self:ListBuffer[String]) = new Other(self)
+
+object Test{
+  def main(args: Array[String]) {
+    
+
+    
+  }
+}
---
+++ src/main/scala/com/reactor/dumbledore/prime/channels/sources/SourceActor.scala
@@ -0,0 +1,48 @@
+package com.reactor.dumbledore.prime.channels.sources
+
+import com.mongodb.DBObject
+import com.mongodb.casbah.Imports._
+import com.reactor.patterns.pull.FlowControlActor
+import com.reactor.patterns.pull.FlowControlArgs
+import com.reactor.store.MongoDB
+import akka.actor.ActorRef
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Future
+import com.fasterxml.jackson.databind.JsonNode
+import scala.concurrent._
+import com.reactor.dumbledore.utilities.Tools
+import com.reactor.dumbledore.data.ListSet
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+class SourceActor(args:FlowControlArgs) extends FlowControlActor(args) {
+  private val NEWS_DB = "reactor-news"
+  private val mongo = new MongoDB
+  ready()
+  
+  override def preStart() = println("SourceActor starting up...")
+  override def postStop() = println("SourceActor terminated.")
+  
+  // Handle messages
+  def receive = {
+    case source_id:String => getNews(source_id, sender)
+    case a:Any => println("Unknown Message - " + a)
+  }
+
+  def getNews(id:String, origin:ActorRef){
+    val query = buildQuery(id)
+    val dataObjects = mongo.find(query, NEWS_DB, 10)
+    val futureNodes = ListBuffer[Future[JsonNode]]()
+    
+    dataObjects map{
+      data => futureNodes += future(Tools.objectToJsonNode(data))
+    }
+    
+    reply(origin, ListSet(id, Await.result(Future.sequence(futureNodes), atMost = 3 seconds)))
+    complete()
+  }
+  
+  def buildQuery(sourceID:String):DBObject = {
+    ("source_id" $eq sourceID)
+  }
+}
